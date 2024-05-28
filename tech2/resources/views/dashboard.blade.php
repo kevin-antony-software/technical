7b951f4a8341a66938f6fbc10807b2ec3acbf9ee
Antony Kevin Abeyesekera <105707336+kevin-antony-software@users.noreply.github.com>
2024-05-28 08:51:36 +0530

    Update dashboard

--- tech2/resources/views/dashboard.blade.php
+++ tech2/resources/views/dashboard.blade.php
@@ -2,32 +2,46 @@
 
     <div>
         <canvas id="myChart"></canvas>
-      </div>
+    </div>
 
-      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
 
-      <script>
-        const ctx = document.getElementById('myChart');
-
+        <script>
+            const ctx = document.getElementById('myChart');
+            Chart.register(ChartDataLabels);
         new Chart(ctx, {
-          type: 'bar',
-          data: {
-            labels: @json($x_axis),
-            datasets: [{
-              label: '# of Jobs Closed this Month',
-              data: @json($y_axis),
-              borderWidth: 1
-            }]
-          },
-          options: {
-            scales: {
-              y: {
-                beginAtZero: true
-              }
+            type: 'bar',
+            data: {
+                labels: @json($x_axis),
+                datasets: [{
+                    label: '# of Jobs Closed this Month',
+                    data: @json($y_axis),
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                plugins: {
+                    datalabels: {
+                        anchor: 'end',
+                        align: 'end',
+                        color: 'blue',
+                        font: {
+                            weight: 'bold',
+                        },
+                        formatter: function(value, context) {
+                            return value;
+                        }
+                    }
+                }
             }
-          }
         });
-      </script>
+    </script>
 
 
 </x-admin.nav>
